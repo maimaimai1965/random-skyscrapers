6b3f8c2319dac7f67b99e4eef632d89470dbcf29
Andrey Miroshnik <maimaimai1965@gmail.com>
2026-07-28 15:44:59 +0200

    Refactor codebase for improved formatting and readability; replace Checkstyle plugin with Spotless for Maven build configuration.

--- src/main/java/SkyscrapersUi.java
+++ src/main/java/SkyscrapersUi.java
@@ -1,11 +1,16 @@
 public interface SkyscrapersUi {
 
-    int showIntroScreen();
-    void showMessage(String message);
-    int showSortScreen(int[] arr);
-    void clearSortScreen();
-    void showNumbersDuringSwap(int[] arr, int k, int j);
-    void replaceElementByNew(int[] arr, int valueOfElement, int newValueOfElement);
-    int getSmallNumber();
+  int showIntroScreen();
 
+  void showMessage(String message);
+
+  int showSortScreen(int[] arr);
+
+  void clearSortScreen();
+
+  void showNumbersDuringSwap(int[] arr, int k, int j);
+
+  void replaceElementByNew(int[] arr, int valueOfElement, int newValueOfElement);
+
+  int getSmallNumber();
 }
